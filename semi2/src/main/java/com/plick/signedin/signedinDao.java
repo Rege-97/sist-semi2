2defe3e8960364e9ac7726cc348751e1b6442b84
xoz0359 <k84890690@gmail.com>
2025-04-07 12:16:19 +0900

    fix: fix errors in queries

--- semi2/src/main/java/com/plick/signedin/signedinDao.java
+++ semi2/src/main/java/com/plick/signedin/signedinDao.java
@@ -25,12 +25,12 @@ public class signedinDao {
 		try {
 			conn = com.plick.db.DBConnector.getConn();
 			String sql = "SELECT name, nickname, tel, email, password, access_type, created_at, "
-					+ "description, membership_members.id, membership_id, member_id, started_at, stopped_at "
+					+ "description, membership_members.membership_id AS membership_id, members.id AS member_id, membership_members.id AS membership_member_id, "
+					+ "started_at, stopped_at "
 					+ "FROM members LEFT OUTER JOIN membership_members "
 					+ "ON members.id = membership_members.member_id "
 					+ "WHERE members.email = ?";
 			pstmt = conn.prepareStatement(sql);
-			System.out.println(sql+dto.getMemberEmail());
 			pstmt.setString(1, dto.getMemberEmail());
 			rs = pstmt.executeQuery();
 			if(rs.next()) {
@@ -42,7 +42,7 @@ public class signedinDao {
 				dto.setMemberAccessType(rs.getString("access_type"));
 				dto.setMemberCreatedAt(rs.getTimestamp("created_at"));
 				dto.setMemberDescription(rs.getString("description"));
-				dto.setId(rs.getInt("id"));
+				dto.setId(rs.getInt("membership_member_id"));
 				dto.setMembershipId(rs.getInt("membership_id"));
 				dto.setMemberId(rs.getInt("member_id"));
 				dto.setMembershipStarted_at(rs.getTimestamp("started_at"));
